a7c9dfc29dbfa3e707b48781205ae9f1f20f3389
CactusField <jonathanpitts2913@gmail.com>
2021-01-14 17:02:52 -0600

    part 2 middleware wont work

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const massive = require('massive');
-const dotenv = require('dotenv');
-const authctrl = require('./controllers/authController');
+const authCtrl = require('./controllers/authController');
+const treasureCtrl = require('./controllers/treasureController');
+const auth = require('./middleware/authMiddleware');
 
-
-const PORT = 4000 || 4000;
+const PORT = 4000;
 
 const { SESSION_SECRET, CONNECTION_STRING } = process.env;
 
@@ -14,26 +14,27 @@ const app = express();
 
 app.use(express.json());
 
-
-
 massive({
-    connectionString: CONNECTION_STRING,
-    ssl: { rejectUnauthorized: false }
-  }).then(db => {
-    app.set('db', db);
-    console.log('db connected');
-  });
-  
-  app.use(
-    session({
-      resave: true,
-      saveUninitialized: false,
-      secret: SESSION_SECRET,
-    })
-  );
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false }
+}).then(db => {
+  app.set('db', db);
+  console.log('db connected');
+});
 
-//endpoints
-app.post('/auth/register', authctrl.register);
-//endpoints
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+  })
+);
 
-app.listen(PORT, ()=>console.log(`All Good on port ${PORT}`))
+app.post('/auth/register', authCtrl.register);
+app.post('/auth/register', authCtrl.login);
+app.get('/auth/register', authCtrl.logout);
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
